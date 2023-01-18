30390cbbcb893c4b23e9a6a0e9cdfbc82b392556
TarumK <kyabishev72@gmail.com>
2023-01-18 20:01:50 +0300

    Запись в базу данных из формы, связанной с моделью

--- appform2/views.py
+++ appform2/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UserForm
+from .models import User
 
 # Create your views here.
 
 
 def index(request):
+    # if request.method != 'POST':
+    #Выводит пустую форму регистрации.
+    # form = UserCreationForm()
     form1 = UserForm()
     return render(request, 'index.html', {'form': form1})
+    # else:
+    #     return HttpResponse('Нажато на сабмит')
+
 
 
 def zapros(request):
     data = request.POST.get('name')
-    return HttpResponse(f'<h1>Привет, {data}!</h1>')
+    data2 = request.POST.get('f_lname')
+    rec = User()
+    rec.lname = data2
+    rec.save()
+    return HttpResponse(f'<h1>Привет, {data} {data2}!</h1>')
